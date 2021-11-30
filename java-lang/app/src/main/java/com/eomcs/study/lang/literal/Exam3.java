d0b19bf51dfafe9c5cd4d1517666d89dca62c42f
JYmingming <sla5962@gmail.com>
2021-11-30 17:38:12 +0900

    test1

--- java-lang/app/src/main/java/com/eomcs/study/lang/literal/Exam3.java
+++ java-lang/app/src/main/java/com/eomcs/study/lang/literal/Exam3.java
@@ -9,20 +9,36 @@ import org.springframework.web.bind.annotation.RestController;
 public class Exam3 {
 
   @GetMapping("/test1")
-  public String test1(){
-    return "정수 : " + 21_0000_0000;
-    //문자열과 숫자를 더하면? 
-    //=>문자열 + (숫자 --> 문자열)= 한개의 문자열로 합친 다.
-  }
-  @GetMapping("/test2")
-  public String test2(){
-    return "정수 : " +  -21_0000_0000;
-    //문자열과 숫자를 더하면? 
-    //=>문자열 + (숫자 --> 문자열)= 한개의 문자열로 합친다.
+  public String test1() {
+    //return "정수: " + 21_4748_3648; // 컴파일 오류! 4 byte 메모리에 저장 X
+    return "정수: " + 21_4748_3647;
+    // 문자열과 숫자를 더하면?  문자열 + (숫자 -> 문자열) = 한 개의 문자열로 합침
   }
 
+  @GetMapping("/test2")
+  public String test2() {
+    //  return "정수: " + -21_4748_3648; // 컴파일 오류! 4 byte 메모리에 저장 X
+    return "정수: " + -21_4748_3648;
+  }
+
+  @GetMapping("/test3")
+  public String test3() {
+    return "정수: " + -21_4748_3649L; // OK! 리터럴을 저장할 때 8byte 메모리 사용하라고 표시
+  }
+
+  @GetMapping("/test4")
+  public String test4() {
+    return "정수: " + -21_4748_3649l; // OK!
+  }
+
+  @GetMapping("/test5")
+  public String test5() {
+    return "정수: " + 922_0000_0000_4748_3648L; // OK!
+  }
+
+  @GetMapping("/test6")
+  public String test6() {
+    return "정수: " + -922_0000_0000_4748_3648L; // OK!
+  }
 
 }
-
-
-
--- java-lang/app/src/main/java/com/eomcs/study/lang/literal/Exam4.java
+++ java-lang/app/src/main/java/com/eomcs/study/lang/literal/Exam4.java
@@ -8,22 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/lang/literal/exam4")
 public class Exam4 {
-
+  //  static float x = 12.375f;
+  //  static double y = 12.375;
 
   @GetMapping("/test1")
   public String test1() {
-
-    float value = 987.654321f;
-    // 문자열을 표현하는 방법
-    return "부동수점 :" +value;
+    float value = 987.654321f; // 4바이트 메모리 사용 → 유효자릿수를 넘어가는 수는 짤린다
+    return "부동소수점: " + value;
   }
 
   @GetMapping("/test2")
   public String test2() {
-    float value = 9876543.21f;
-    // 문자열을 표현하는 방법
-    return "부동수점 :" +value*100;
+    double value = 987.654321; // 8바이트 메모리 사용
+    return "부동소수점: " + value;
   }
-
-
 }
