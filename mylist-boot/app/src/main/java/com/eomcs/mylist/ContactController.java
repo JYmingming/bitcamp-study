25957ccff5913f1a516326ee89136f437bd01cb1
JYmingming <sla5962@gmail.com>
2021-12-13 16:36:11 +0900

    20211213

--- mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
@@ -1,39 +1,39 @@
+
 package com.eomcs.mylist;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // 클라이언트 요청을 처리하는 역할
 public class ContactController {
-
-  // 인스턴스 변수
-  // => 모든 인스턴스 메서드가 공유한다. 
-  String[] contacts = new String[5];
-  int size = 0;
-
-  @GetMapping("/contact/list")
+  String[] contacts =new String[5];
+  int size=0;
+  @RequestMapping("/contact/list")
   public Object list() {
-    String[] records = new String[size];
-    for (int i = 0; i < size; i++) {
-      records[i] = contacts[i];
-    }
-    return records;
+    String[] contacts = {
+        "aaa1,aaa@test.com,1111,비트캠프",
+        "aaa1,aaa@test.com,1111,비트캠프",
+        "aaa1,aaa@test.com,1111,비트캠프",
+        "aaa1,aaa@test.com,1111,비트캠프",
+        "aaa1,aaa@test.com,1111,비트캠프"
+    };
+    return contacts;
   }
-
-  @GetMapping("/contact/add")
-  public Object add(String name, String email, String tel, String company) {
-    contacts[size++] = name + "," + email + "," + tel + "," + company;
-    return size;
+  @RequestMapping("/contact/add")
+  public Object add(String name,String email,String tel, String company) {
+    String contact = name + ","+ email + "," + tel + "," + company;
+    contacts[size++] = contact;
+    return contact; 
   }
-
-  @GetMapping("/contact/get")
+  @RequestMapping("/contact/get")
   public Object get(String email) {
-    for (int i = 0; i < size; i++) {
-      if (email.equals(contacts[i].split(",")[1])) {
-        return contacts[i];
+    for(int i=0; i<size; i++) {
+      if(contacts[i].split(",")[1].equals(email)) {
+        return contacts[i]; 
       }
+
     }
     return "";
-  }
+  } 
+}  
 
-}
