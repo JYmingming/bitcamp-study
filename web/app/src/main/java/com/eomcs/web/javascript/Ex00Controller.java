d88aa5c8c0b95c11c48eb9961e69c8af3a50d02e
JYmingming <sla5962@gmail.com>
2021-12-15 14:49:25 +0900

    20211215

---
+++ web/app/src/main/java/com/eomcs/web/javascript/Ex00Controller.java
@@ -0,0 +1,22 @@
+package com.eomcs.web.javascript;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/javascript/ex00/exam06")
+public class Ex00Controller {
+
+
+  @RequestMapping("/javascript/ex00/exam06")
+  public String exam06() {
+    return "<ul><li>홍길동</li><li>홍길동</li><li>홍길동</li></ul>";
+
+  }
+
+
+
+
+
+
+
+
+}
---
+++ web/app/src/main/java/com/eomcs/web/javascript/Ex01Controller.java
@@ -0,0 +1,23 @@
+package com.eomcs.web.javascript;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Ex01Controller {
+
+
+  @RequestMapping("/javascript/ex01/exam05_1")
+  public Object exam05_1() {
+    return "window.alert('okok')";
+
+  }
+  @RequestMapping("/javascript/ex01/exam05_2")
+  public Object exam05_2() {
+    return "window.alert('nono')";
+
+  }
+
+
+
+}
