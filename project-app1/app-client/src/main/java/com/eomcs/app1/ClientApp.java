457a3f9003fbed7afc531630b3041f2564bf791c
JYmingming <sla5962@gmail.com>
2022-01-27 15:31:59 +0900

    20220127

--- project-app1/app-client/src/main/java/com/eomcs/app1/ClientApp.java
+++ project-app1/app-client/src/main/java/com/eomcs/app1/ClientApp.java
@@ -7,32 +7,41 @@ import java.util.Scanner;
 public class ClientApp {
 
   public static void main(String[] args) throws Exception {
+    System.out.println("[계산기 클라이언트]");
 
     Scanner keyScan = new Scanner(System.in);
 
-    Socket socket = new Socket("localhost", 9999);
+    //1) 서버 애플리케이션과 네트워크 연결을 수행한다.
+    Socket socket = new Socket("localhost", 8888);
     System.out.println("서버와 연결됨!");
 
-    Scanner in = new Scanner(socket.getInputStream());
+
+    //데이터를 주고 받기 위한 입출력 스트림을 준비한다.
     PrintStream out = new PrintStream(socket.getOutputStream());
+    Scanner in = new Scanner(socket.getInputStream());
 
-    while (true) {
-      System.out.print("> ");
-      String input = keyScan.nextLine();
-      out.println(input);
+    //서버에데이터를 보낸다.
+    out.println("강사");
+    String response = in.nextLine();
+    System.out.println("==>"+ response);
 
-      if (input.equals("quit") || input.equals("exit")) {
-        break;
-      }
-
-      while (true) {
-        String result = in.nextLine();
-        if (result.equals("")) {
-          break;
-        }
-        System.out.println(result);
-      }
-    }
+    //    while (true) {
+    //      System.out.print("> ");
+    //      String input = keyScan.nextLine();
+    //      out.println(input);
+    //
+    //      if (input.equals("quit") || input.equals("exit")) {
+    //        break;
+    //      }
+    //
+    //      while (true) {
+    //        String result = in.nextLine();
+    //        if (result.equals("")) {
+    //          break;
+    //        }
+    //        System.out.println(result);
+    //      }
+    //    }
 
     socket.close();
     keyScan.close();
--- project-app1/app-server/src/main/java/com/eomcs/app1/ServerApp.java
+++ project-app1/app-server/src/main/java/com/eomcs/app1/ServerApp.java
@@ -10,12 +10,18 @@ public class ServerApp {
 
   public static void main(String[] args) throws Exception {
 
-    ServerSocket serverSocket = new ServerSocket(9999);
-    System.out.println("서버 실행!");
-    System.out.println("클라이언트 연결을 기다리는 중...");
 
-    Socket socket = serverSocket.accept();
+    //1) 클라이언트 App의 연결을 준비한다
+    ServerSocket serverSocket = new ServerSocket(8888);
+    System.out.println("서버 실행!");
+
+    //2) 클라이언트의 연결 요청을 승인한다.
+    // - 리턴값은 클라이언트와 연결된 정보 
+    System.out.println("클라이언트 연결을 기다리는 중...");
+    Socket socket = serverSocket.accept(); // 클라이언트가 연결될 때까지 리턴하지 않는다.
     System.out.println("클라이언트와 연결됨!");
+
+
 
     Scanner in = new Scanner(socket.getInputStream());
     PrintStream out = new PrintStream(socket.getOutputStream());
@@ -51,9 +57,14 @@ public class ServerApp {
       out.println();
     }
 
+
+    //클라이언트와의 연결을 끊음
     socket.close();
+    System.out.println("클라이언트 연결종료!");
+
+    //서버쪽 연결도구 종료!
     serverSocket.close();
-    System.out.println("Goodbye!");
+    System.out.println("서버 종료!");
   }
 
 }
