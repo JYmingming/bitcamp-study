e560832c23ece899110c4f2f4b8688d5341f4e4a
JYmingming <sla5962@gmail.com>
2022-01-31 17:43:20 +0900

    20220131

--- project-app1/app-server/src/main/java/com/eomcs/app1/ServerApp.java
+++ project-app1/app-server/src/main/java/com/eomcs/app1/ServerApp.java
@@ -8,23 +8,23 @@ public class ServerApp {
   public static void main(String[] args) throws Exception {
     System.out.println("[계산기 서버]");
 
-    // 1) 클라이언트 App의 연결을 준비한다.
+    // 클라이언트 App의 연결을 준비한다.
     ServerSocket serverSocket = new ServerSocket(8888);
 
     while (true) {
-
-      // 2) 클라이언트의 연결 요청을 승인한다.
-      //    - 리턴 값은 클라이언트와 연결된 정보
       System.out.println("클라이언트의 연결을 기다림!");
-      Socket socket = serverSocket.accept(); // 클라이언트가 연결될 때까지 리턴하지 않는다. 기다린다.
+      Socket socket = serverSocket.accept(); 
       System.out.println("클라이언트와 연결됨!");
 
+      // 클라이언트의 요청을 처리할 작업자를 만든다.
       Worker worker = new Worker(socket);
-      worker.start();
-      System.out.println("작업자에게 클라이언트 요청을 맡겼");
+      worker.start(); // 작업자에게 일을 시킨 후 즉시 리턴한다.
+      System.out.println("작업자에게 클라이언트 요청을 맡겼음!");
     }
-    // 클라이언트와의 연결을 끊음
+
+    // 서버쪽 연결 도구 종료!
+    //    serverSocket.close();
+    //    System.out.println("서버 종료!");
   }
 
 }
-
