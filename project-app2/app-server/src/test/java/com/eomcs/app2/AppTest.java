08b45504733cc6863d23eed4015c5cbc326a1f89
JYmingming <sla5962@gmail.com>
2022-02-18 14:33:15 +0900

    20220216

---
+++ project-app2/app-server/src/test/java/com/eomcs/app2/AppTest.java
@@ -0,0 +1,8 @@
+package com.eomcs.app2;
+
+public class AppTest {
+  //    @Test public void appHasAGreeting() {
+  //        App classUnderTest = new App();
+  //        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+  //    }
+}
