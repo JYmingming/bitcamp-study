6e51022c0874aa39873942331e309d16819547bf
JYmingming <sla5962@gmail.com>
2022-02-21 09:50:46 +0900

    20220221

---
+++ mylist-boot/app/src/main/java/com/eomcs/io/test.java
@@ -0,0 +1,31 @@
+package com.eomcs.io;
+
+import java.util.Scanner;
+
+public class test {
+  public static void main(String[] args) throws Exception {
+    System.out.println("[계산기 클라이언트]");
+
+    Scanner keyScan = new Scanner(System.in);
+    String x ="";
+    while(true) {
+      String input = keyScan.nextLine();
+      switch (input) {
+        case "":
+
+          break;
+
+        default:
+          break;
+      }
+      x =input;
+      System.out.println(x);
+    }
+
+
+
+
+  }
+
+}
+}
