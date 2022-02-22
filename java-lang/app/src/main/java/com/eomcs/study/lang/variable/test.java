50b9aed80e608ccb298933083ae661cb83adb7fe
JYmingming <sla5962@gmail.com>
2022-02-22 14:32:29 +0900

    20220222

---
+++ java-lang/app/src/main/java/com/eomcs/study/lang/variable/test.java
@@ -0,0 +1,13 @@
+package com.eomcs.study.lang.variable;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class test {
+  public static void main(String[] args) throws Exception{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int tc = Integer.parseInt(br.readLine());
+
+
+  }
+}
