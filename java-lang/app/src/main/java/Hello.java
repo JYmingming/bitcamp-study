ec7161b49adc1a8a824b9f277937df9984c72463
JYmingming <sla5962@gmail.com>
2022-03-03 14:45:54 +0900

    202203035

--- java-lang/app/src/main/java/Hello.java
+++ java-lang/app/src/main/java/Hello.java
@@ -4,7 +4,6 @@ public class Hello {
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     System.out.println("Hello world!");
-    System.out.println("Hello world!");
   }
 
 }
