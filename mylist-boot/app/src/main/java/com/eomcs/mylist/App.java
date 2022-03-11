0feb17abd38ec5af9c860c67ed7ca076b6dcce04
JYmingming <sla5962@gmail.com>
2022-03-11 20:42:51 +0900

    build

--- mylist-boot/app/src/main/java/com/eomcs/mylist/App.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/App.java
@@ -1,5 +1,6 @@
 package com.eomcs.mylist;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class App {
+
+  @Value("${spring.datasource.driver-class-name}")
+  String driverClassName;
+  @Value("${spring.datasource.url}")
+  String url;
+  @Value("${spring.datasource.username}")
+  String username;
+  @Value("${spring.datasource.password}")
+  String password;
+
+
+
 
   public static void main(String[] args) {
     SpringApplication.run(App.class, args);
