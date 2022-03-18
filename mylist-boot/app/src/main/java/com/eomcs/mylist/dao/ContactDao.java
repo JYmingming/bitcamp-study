1f7e1e30af67eb16d5b716ef7b60400e896cbee0
JYmingming <sla5962@gmail.com>
2022-03-19 01:30:31 +0900

    test2

--- mylist-boot/app/src/main/java/com/eomcs/mylist/dao/ContactDao.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/dao/ContactDao.java
@@ -3,11 +3,12 @@ package com.eomcs.mylist.dao;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import com.eomcs.mylist.domain.Contact;
+import com.eomcs.mylist.domain.ContactTel;
 
 @Mapper
 public interface ContactDao {
 
-  int countAll(); // ==> <select id="com.eomcs.mylist.dao.ContactDao.countAll">...</select> 
+  int countAll(); 
 
   List<Contact> findAll();
 
@@ -22,6 +23,16 @@ public interface ContactDao {
   int update(Contact contact);
 
   int delete(int no);
+
+  List<ContactTel> findTelByContactNo(int contactNo);
+
+  int insertTel(ContactTel tel);
+
+  int updateTel(ContactTel tel);
+
+  int deleteTel(int telNo);
+
+  int deleteTelByContactNo(int contactNo);
 }
 
 
